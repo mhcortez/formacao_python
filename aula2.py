ffe016f1eece4766ad63aead09c9fc4572c3b2d8
mhcortez <mhcortez@gmail.com>
2024-09-24 20:51:30 -0300

    Update aula2.py
    alteracao do arquivo

---
+++ aula2.py
@@ -0,0 +1,21 @@
+#Segunda aula
+#Sintaxe básica de python
+
+string = "Maria"
+inteiro = 30
+flutuante = 1.65
+
+lista = [1,2,3,4,5] #Mutável
+tupla = (1,2,3,4,5) #Imutavel
+
+dicionario = {'nome':'joão', 'idade': 30, 'valor': 3.14}
+
+boleano = False
+
+print(type(string))
+print(type(inteiro))
+print(type(flutuante))
+print(type(lista))
+print(type(tupla))
+print(type(dicionario))
+print(type(boleano))
