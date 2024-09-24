3a40df61c4738b5c20bd36e5763cd054c4e878ff
mhcortez <mhcortez@gmail.com>
2024-09-24 20:51:49 -0300

    Create aula2_1.py
    novo arquivo com processamento

---
+++ aula2_1.py
@@ -0,0 +1,11 @@
+#Entrada de dados
+
+salarioBase = float(input("Salario Base: "))
+gratificacao = float(input("Gratificação: "))
+
+#processamento
+salarioBruto = salarioBase + gratificacao
+
+#saida
+print("O salario total é R$:", salarioBruto)
+print(f"O salario bruto é R$: {salarioBruto}")
