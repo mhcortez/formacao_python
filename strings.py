59a035467a341aab5db2e25b213787b27a70c056
mhcortez <mhcortez@gmail.com>
2024-09-27 20:43:41 -0300

    Aula 5
    Alteraçao String

--- strings.py
+++ strings.py
@@ -11,3 +11,10 @@ print(palavras)
 mensagem = "Hello, World!"
 if "World" in mensagem:
     print("A substring 'world' esta presente na mensagem")
+
+mensagem = "Hello, World!"
+mensagem = mensagem.replace("Hello", "Python")
+print(mensagem)
+
+mensagem = " Hello, World ! "
+print(mensagem.strip())
